ac9f3667111a0672a114dc39834acb578eed678c
keitakurita <keita.kurita@gmail.com>
2018-02-09 19:01:46 +0900

    Further modifications to setup.py

--- setup.py
+++ setup.py
@@ -9,19 +9,18 @@ extensions = [
 
 setup(                                                                        
     name='pycmf',
-    version='1.0.0',
+    version='0.9.0',
     packages=['pycmf'],
     include_dirs = [numpy.get_include()],
     ext_modules=extensions,
     cmdclass={'build_ext': build_ext},
-    url='https://github.com/keitakurita/CMF_Python',
+    url='https://github.com/keitakurita/smn-ailab/PyCMF',
     license='MIT',
     author='keitakurita',
     author_email='keita.kurita@gmail.com',
     description='A library for collective matrix factorization in Python.',   
     install_requires=['scipy', 'numpy', 'scikit-learn'],
     classifiers=[
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ]
 )
