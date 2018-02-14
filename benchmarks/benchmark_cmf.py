0f06150a0559ba5caff5bd1e7699c7cc1d229ab5
keitakurita <keita.kurita@gmail.com>
2018-02-14 16:58:22 +0900

    BUG: Fixed benchmark script to finish in limited time

--- benchmarks/benchmark_cmf.py
+++ benchmarks/benchmark_cmf.py
@@ -2,13 +2,15 @@ import numpy as np
 from scipy.sparse import csc_matrix
 from scipy.special import expit
 import timeit
-import sys; sys.path.append("../..")
-from CMF import CMF
+import sys; sys.path.append("..")
+from pycmf import CMF
 
 import argparse
 parser = argparse.ArgumentParser(description='Benchmark CMF')
 parser.add_argument('--runs', '-r', type=int, default=1,
                     help='The number of runs per experiment')
+parser.add_argument('--worst', '-w', action='store_true',
+                    help='Whether to print worst performance or not')
 args = parser.parse_args()
 
 class BenchmarkCase:
@@ -19,10 +21,17 @@ class BenchmarkCase:
         
     def run(self):
         print("-" * 75)
-        t = timeit.Timer(f"{self.function_name}({self.arguments})",
-                         setup=f"from __main__ import {self.function_name}")
-        best_run = min(t.repeat(args.runs))
-        print(f"{self.test_name}: Best of {args.runs} runs was {best_run} sec.")
+        run_hist = timeit.repeat(f"{self.function_name}({self.arguments})",
+                                     setup=f"from __main__ import {self.function_name}",
+                                     number=1,
+                                     repeat=args.runs)
+        best_run = min(run_hist)
+        msg = "{}: Best of {} runs was {:.2f} sec.".format(self.test_name, 
+                                                          args.runs,
+                                                          best_run)
+        if args.worst:
+            msg += ", worst was {:.2f} sec.".format(max(run_hist))
+        print(msg)
 
 def dense_cmf_benchmark(solver):
     rng = np.random.mtrand.RandomState(42)
@@ -62,13 +71,13 @@ def sparse_cmf_with_logits_benchmark():
                 random_state=42)
     U, V, Z = model.fit_transform(X, Y)
 
-if __name__ == "__main__":
+if __name__ == "__main__":        
     print("=" * 75)
     print("Commencing benchmark...")
-    for solver in ["mu", "newton"]:
+    for solver in ["mu", "mu"]:
         BenchmarkCase(f"Dense CMF, {solver}", "dense_cmf_benchmark", arguments=f"'{solver}'").run()
     BenchmarkCase(f"Dense CMF with logits", "dense_cmf_with_logits_benchmark").run()
-    for solver in ["mu", "newton"]:
+    for solver in ["mu", "mu"]:
         BenchmarkCase(f"Sparse CMF, {solver}", "sparse_cmf_benchmark", arguments=f"'{solver}'").run()
     BenchmarkCase(f"Sparse CMF with logits", "sparse_cmf_with_logits_benchmark").run()
     print("=" * 75)
