cd02b4878be33639619ca9dc91d8224617bc2420
keitakurita <keita_kurita@administrator-no-MacBook.local>
2018-03-02 15:56:54 +0900

    BUG: Fixed bugs and styling for benchmark script

--- benchmarks/benchmark_cmf.py
+++ benchmarks/benchmark_cmf.py
@@ -12,25 +12,27 @@ parser.add_argument('--worst', '-w', action='store_true',
                     help='Whether to print worst performance or not')
 args = parser.parse_args()
 
+
 class BenchmarkCase:
     def __init__(self, test_name, function_name, arguments=""):
         self.test_name = test_name
         self.function_name = function_name
         self.arguments = arguments
-        
+
     def run(self):
         print("-" * 75)
         run_hist = timeit.repeat(f"{self.function_name}({self.arguments})",
-                                     setup=f"from __main__ import {self.function_name}",
-                                     number=1,
-                                     repeat=args.runs)
+                                 setup=f"from __main__ import {self.function_name}",
+                                 number=1,
+                                 repeat=args.runs)
         best_run = min(run_hist)
-        msg = "{}: Best of {} runs was {:.2f} sec.".format(self.test_name, 
-                                                          args.runs,
-                                                          best_run)
+        msg = "{}: Best of {} runs was {:.2f} sec.".format(self.test_name,
+                                                           args.runs,
+                                                           best_run)
         if args.worst:
             msg += ", worst was {:.2f} sec.".format(max(run_hist))
         print(msg)
+
 
 def dense_cmf_benchmark(solver):
     rng = np.random.mtrand.RandomState(42)
@@ -40,6 +42,7 @@ def dense_cmf_benchmark(solver):
                 random_state=42)
     U, V, Z = model.fit_transform(X, Y)
 
+
 def dense_cmf_with_logits_benchmark():
     rng = np.random.mtrand.RandomState(42)
     X = np.abs(rng.randn(2000, 150))
@@ -47,6 +50,7 @@ def dense_cmf_with_logits_benchmark():
     model = CMF(n_components=10, solver="newton",
                 random_state=42)
     U, V, Z = model.fit_transform(X, Y)
+
 
 def sparse_cmf_benchmark(solver):
     rng = np.random.mtrand.RandomState(42)
@@ -57,7 +61,8 @@ def sparse_cmf_benchmark(solver):
     Y = np.abs(rng.randn(150, 10))
     model = CMF(n_components=10, solver=solver,
                 random_state=42)
-    U, V, Z = model.fit_transform(X, Y)
+    U, V, Z = model.fit_transform(X_sparse, Y)
+
 
 def sparse_cmf_with_logits_benchmark():
     rng = np.random.mtrand.RandomState(42)
@@ -68,15 +73,18 @@ def sparse_cmf_with_logits_benchmark():
     Y = expit(rng.randn(150, 10))
     model = CMF(n_components=10, solver="newton",
                 random_state=42)
-    U, V, Z = model.fit_transform(X, Y)
+    U, V, Z = model.fit_transform(X_sparse, Y)
 
-if __name__ == "__main__":        
+
+if __name__ == "__main__":
     print("=" * 75)
     print("Commencing benchmark...")
-    for solver in ["mu", "mu"]:
-        BenchmarkCase(f"Dense CMF, {solver}", "dense_cmf_benchmark", arguments=f"'{solver}'").run()
+    for solver in ["mu", "newton"]:
+        BenchmarkCase(f"Dense CMF, {solver}", "dense_cmf_benchmark",
+                      arguments=f"'{solver}'").run()
     BenchmarkCase(f"Dense CMF with logits", "dense_cmf_with_logits_benchmark").run()
-    for solver in ["mu", "mu"]:
-        BenchmarkCase(f"Sparse CMF, {solver}", "sparse_cmf_benchmark", arguments=f"'{solver}'").run()
+    for solver in ["mu", "newton"]:
+        BenchmarkCase(f"Sparse CMF, {solver}", "sparse_cmf_benchmark",
+                      arguments=f"'{solver}'").run()
     BenchmarkCase(f"Sparse CMF with logits", "sparse_cmf_with_logits_benchmark").run()
     print("=" * 75)
